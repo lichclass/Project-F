dfb1abb90458b3a8be4f1159619318305af6ac1f
Nash Adam Munoz <150409949+lichclass@users.noreply.github.com>
2024-09-02 21:53:08 +0800

    Project-F Documents Page
    Created the Docx page for easier navigation

--- ProjectFDocs/src/components/Card.jsx
+++ ProjectFDocs/src/components/Card.jsx
@@ -1,11 +1,37 @@
-function Card() {
+import PropTypes from 'prop-types';
+
+function Card(props) {
   return (
-    <div className="card">
-      <img src="https://via.placeholder.com/150" alt="Placeholder" />
-      <h2>Card Title</h2>
-      <p>Card Description</p>
+    <div className="flex border rounded w-72 max-h-max p-4
+                    shadow-lg hover:border-blue-700 hover:shadow-xl
+                    transform hover:scale-105 transition-transform duration-300
+                    cursor-pointer">
+      <a href={props.link} target="_blank" rel="noopener noreferrer">
+        <div className="flex items-center justify-center p-4">
+          <img src={props.image} className="mx-auto" />
+        </div>
+        <div className='text-center p-5'>
+          <span className="text-2xl font-bold">{props.title}</span>
+          <br /><br /><hr />
+          <span>{props.description}</span>
+        </div>
+      </a>
     </div>
   );
+}
+
+Card.propTypes = {
+    link: PropTypes.string,
+    image: PropTypes.string,
+    title: PropTypes.string,
+    description: PropTypes.string,
+}
+
+Card.defaultProps = {
+    link: "#",
+    image: "https://via.placeholder.com/150",
+    title: "Card Title",
+    description: "Card Description",
 }
 
 export default Card;
